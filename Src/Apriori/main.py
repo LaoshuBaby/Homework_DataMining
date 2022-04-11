c08f840c1ddc20fce44cd205762e22da2c55bfbe
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2022-04-11 12:11:25 +0800

    Update main.py
    勉强能用
    修改split

--- Src/Apriori/main.py
+++ Src/Apriori/main.py
@@ -1,4 +1,4 @@
-FILE_PATH=""
+FILE_PATH="D:\\retail.dat"
 TID=0
 RAW_DATA=[]
 
@@ -7,6 +7,7 @@ MIN_SUP=0.5
 ## INPUT DATA
 
 def genTID():
+    global TID
     TID+=1
     return TID
 
@@ -15,6 +16,7 @@ def read():
     dat_text=dat_file.read()
     for i in range(len(dat_text)):
         RAW_DATA.append([genTID(),dat_text[i].split(" ")])
+    return RAW_DATA
 
 ## COMPUTE
 
@@ -29,6 +31,11 @@ def build_first_c_list(raw_data):
             c_list.add(raw_data[i][1][j])
     c_list=list(c_list)
     return c_list
+
+#### COUNT
+
+
+#### GenCi
 
 def count_sup_for_C_list(raw_data, c_list):
     for i in range(len(raw_data)):
@@ -45,19 +52,18 @@ def c_list_prune(c_list, MIN_SUP):
             new_c_list.append(c_list[i])
     return new_c_list
 
+
 def build_pre_l_list(c_list):
     # 构建出所有可能的子集组合
     return l_list
-    pass
+
+#### PruneLi
 
 def l_list_prune(l_list,c_list):
     # 输入对应的clist，对llist的每一个项集都拆开看其子集是否全都在clist里，有不合法的就毙掉这个llist中的项集
     return true_l_list
-    pass
-
-#### COUNT
 
 
-#### GenCi
-
-#### PruneLi
+if __name__ == "__main__":
+    raw_data=read()
+    print(raw_data)
