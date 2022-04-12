0c71546cfa84210caf9d42939ee9d96a39d071fe
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2022-04-13 01:36:02 +0800

    Failed to build l1

--- Src/Apriori/main.py
+++ Src/Apriori/main.py
@@ -37,7 +37,17 @@ def c_list_prune(c_list, MIN_SUP):
 
 
 def build_pre_l_list(c_list):
-    # 构建出所有可能的子集组合
+    # 构建出所有可能的子集的组合,目前这步有重复
+    
+    l_list=[]
+    for i in range(len(c_list)):
+        for j in range(len(c_list)):
+            if i!=j:
+                l_list.append(Itemset(
+                    data=set(c_list[i].data).union(set(c_list[j].data)),
+                    sup=0,
+                    count=0
+                ))
     return l_list
 
 #### PruneLi
@@ -47,6 +57,7 @@ def l_list_prune(l_list,c_list):
     for i in range(len(l_list)):
         flag_not_exist=False
         for j in range(len(c_list)):
+            print("i="+str(i)+" j="+str(j))
             if set(c_list[j].data).issubset(set(l_list[i].data)) == False:
                 flag_not_exist = True
                 break
@@ -57,17 +68,26 @@ def l_list_prune(l_list,c_list):
 
 def apriori(RAW_DATA, MIN_SUP):
     c1=build_first_c_list(RAW_DATA)
-    for i in range(len(c1)):
-        print(c1[i])
-    print("======")
-    c1=count_sup_for_C_list(RAW_DATA,c1)
-    for i in range(len(c1)):
-        print(c1[i])
-    print("======")
-    c1=c_list_prune(c1,MIN_SUP)
+    # for i in range(len(c1)):
+    #     print(c1[i])
+    # print("======")
+    #c1=count_sup_for_C_list(RAW_DATA,c1)
+    # for i in range(len(c1)):
+    #     print(c1[i])
+    # print("======")
+    # 除了前面的一句预热，其他的都可以循环内执行了
+    c1=c_list_prune(count_sup_for_C_list(RAW_DATA,c1),MIN_SUP)
     for i in range(len(c1)):
         print(c1[i])
     # 按理说L1直接等于C1，但是我们还是走一下生成候选项集的过程，拓展组合然后prune会这个和C1一样的L1
+    print("======","len(c1)="+str(len(c1)))
+    l1=build_pre_l_list(c1)
+    for i in range(len(l1)):
+        print(l1[i])
+    print("======","len(l1)="+str(len(l1)))
+    l1=l_list_prune(l1,c1)
+    for i in range(len(l1)):
+        print(l1[i])
 
 if __name__ == "__main__":
     exit(-1)
