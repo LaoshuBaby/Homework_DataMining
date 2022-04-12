455162bdc223552695559801a1b9848f2cf09f12
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2022-04-13 00:41:39 +0800

    C1 successful
    data: ['1'] sup :0.5 Count :2
    data: ['3'] sup :0.75 Count :3
    data: ['5'] sup :0.75 Count :3
    data: ['2'] sup :0.75 Count :3

--- Src/Apriori/main.py
+++ Src/Apriori/main.py
@@ -19,10 +19,14 @@ def build_first_c_list(raw_data):
 def count_sup_for_C_list(raw_data, c_list):
     for i in range(len(raw_data)):
         for j in range(len(c_list)):
-            if is_in(raw_data[i][1],c_list[j].data) :
+            if set(c_list[j].data).issubset(set(raw_data[i][1])):
+            # if is_in(raw_data[i][1],c_list[j].data) :
                 c_list[j].count+=1
     for i in range(len(c_list)):
         c_list[i].sup=c_list[i].count/len(raw_data)
+    # 这个地方最好能对c_list进行排序，按照data里面各个元素的字典序
+    # c_list=
+    return c_list
 
 def c_list_prune(c_list, MIN_SUP):
     new_c_list=[]
@@ -55,7 +59,15 @@ def apriori(RAW_DATA, MIN_SUP):
     c1=build_first_c_list(RAW_DATA)
     for i in range(len(c1)):
         print(c1[i])
-
+    print("======")
+    c1=count_sup_for_C_list(RAW_DATA,c1)
+    for i in range(len(c1)):
+        print(c1[i])
+    print("======")
+    c1=c_list_prune(c1,MIN_SUP)
+    for i in range(len(c1)):
+        print(c1[i])
+    # 按理说L1直接等于C1，但是我们还是走一下生成候选项集的过程，拓展组合然后prune会这个和C1一样的L1
 
 if __name__ == "__main__":
     exit(-1)
